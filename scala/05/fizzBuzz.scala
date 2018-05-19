1a129fe244aa426d0bb4fb81cbe3c290d8cf2e9d
Ryan Shin <1982sdr@hanmail.net>
2018-05-19 15:02:47 +0900

    scala - phase 05 modifieid.

--- scala/05/fizzBuzz.scala
+++ scala/05/fizzBuzz.scala
@@ -1,11 +1,24 @@
 object fizzBuzz extends App {
-  def fizzBuzz(n: Int)(cond: (Int, String)*): Unit = {
+  // teacher's solution
+  def fizzBuzz(n: Int)(cond: (Int, String)*): Unit = 1 to n map { i =>
+    cond.foldLeft(""){ case (acc, (x, s)) => if (i % x == 0) acc + s else acc } match {
+      case "" => i.toString
+      case other => other
+    }
+  } foreach println
+
+  // my solution
+  def fizzBuzz2(n: Int)(cond: (Int, String)*): Unit = {
     for(i <- 1 to n) {
       val res = cond.filter( i % _._1 == 0 )
       val s = res.map(_._2).mkString
-      if(res.map(_._2).length > 0) println( s"$i = $s" )
+      if(res.map(_._2).length == 0) println(s"$i") else println( s"$s" )
     }
   }
 
+  println("Teacher's Solution");
   fizzBuzz(20)((3, "Fizz"), (5, "Buzz"))
+  println();
+  println("My Solution");
+  fizzBuzz2(20)((3, "Fizz"), (5, "Buzz"))
 }
