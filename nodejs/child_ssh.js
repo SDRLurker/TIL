69f71f7e450bebae45f75a95c32cc720e5e1b673
Ryan Shin <1982sdr@hanmail.net>
2019-03-25 00:06:39 +0900

    nodejs - modify child_ssh.js

--- nodejs/child_ssh.js
+++ nodejs/child_ssh.js
@@ -1,6 +1,14 @@
+if(process.argv.length != 4) {
+  console.log("Usage node child_ssh.js IP CMD");
+  process.exit(-1);
+}
+const ip = process.argv[2];
+const cmd = process.argv[3];
 const { spawn } = require('child_process');
-// ssh ??? 접속
-const child = spawn('ssh', ['???']);
+// ssh IP 접속
+const child = spawn('ssh', ["-tt", ip]);
+
+console.log(process.argv.length);
 
 child.on('exit', function (code, signal) {
   console.log('child process exited with ' +
@@ -10,11 +18,11 @@ child.on('exit', function (code, signal) {
 child.stdout.on('data', (data) => {
   console.log(`child stdout:\n${data}`);
   // 화면에서 login ok가 출력되면
-  if( data.indexOf("login ok") > -1) {
+  if( data.indexOf("login") > -1 ) {
     // 3초마다 http, https 접속 확인 shell 명령 전송.
     // \n이 키보드에 엔터키 기능이라 필수적으로 들어가야 함!
     setInterval( () => {
-      child.stdin.write("pwd\n");
+      child.stdin.write(cmd + "\n");
     }, 3000)
   }
 });
