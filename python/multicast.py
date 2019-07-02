46dd124fe999a9c5a346c01f2973d33ab0032b7e
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2019-07-02 22:39:42 +0900

    python - multicast.py insert MCAST_GRP

--- python/multicast.py
+++ python/multicast.py
@@ -11,8 +11,8 @@ from time import sleep
 def gen_multicast_sock(MCAST_GRP, MCAST_PORT):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
-                                     # to MCAST_GRP, not all groups on MCAST_PORT
+    sock.bind((MCAST_GRP, MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
+                                        # to MCAST_GRP, not all groups on MCAST_PORT
     mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
@@ -25,7 +25,7 @@ def recv_one(sock):
         print(len(s), s)
     except socket.error as e:
         err = e.args[0]
-        if err == errno.EAGAIN or err == errno.EWORLDBLOCK:
+        if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
             sleep(0.001)
             return
         else:
