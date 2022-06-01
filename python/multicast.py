63fe8b0977cd21a72df3fa06f1c49455f0e6779e
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2022-06-01 19:49:09 +0900

    python - modify multicast.py

--- python/multicast.py
+++ python/multicast.py
@@ -8,16 +8,23 @@ import sys
 import errno
 from time import sleep
 
-def gen_multicast_sock(MCAST_GRP, MCAST_PORT):
+
+# https://stackoverflow.com/questions/603852/how-do-you-udp-multicast-in-python
+def gen_multicast_sock(MCAST_GRP, MCAST_PORT, nicip='0.0.0.0'):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((MCAST_GRP, MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
-                                        # to MCAST_GRP, not all groups on MCAST_PORT
-    mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
-
+    # use MCAST_GRP instead of '' to listen only
+    # to MCAST_GRP, not all groups on MCAST_PORT
+    sock.bind((MCAST_GRP, MCAST_PORT))
+    MCAST_GRPIP = socket.inet_aton(MCAST_GRP)
+    if nicip == '0.0.0.0':
+        mreq = struct.pack("4sl", MCAST_GRPIP, socket.INADDR_ANY)
+    else:
+        mreq = struct.pack("4s4s", MCAST_GRPIP, socket.inet_aton(nicip))
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
     fcntl.fcntl(sock, fcntl.F_SETFL, os.O_NONBLOCK)
     return sock
+
 
 def recv_one(sock):
     try:
@@ -32,14 +39,16 @@ def recv_one(sock):
             print(e)
             sys.exit(1)
 
+
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 3:
-        print("Usage) %s IP PORT" % sys.argv[0])
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        print("Usage) %s IP PORT (NICIP)" % sys.argv[0])
         sys.exit(-1)
     ip = sys.argv[1]
     port = int(sys.argv[2])
-    sock1 = gen_multicast_sock(ip,port)
+    nicip = sys.argv[3] if len(sys.argv) >= 4 else '0.0.0.0'
+
+    sock1 = gen_multicast_sock(ip, port, nicip)
 
     while True:
         recv_one(sock1)
